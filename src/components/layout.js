f92221cbe23052d1223d9718e7569a1ec1f6e9ee
romanca <r.jamrisko@gmail.com>
2021-09-07 14:31:59 +0200

    layout

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div
+      style={{
+        height: 600,
+        width: 400,
+        border: "1px solid black",
+        borderRadius: 10,
+      }}
+    ></div>
+  );
+};
+
+export default Layout;
