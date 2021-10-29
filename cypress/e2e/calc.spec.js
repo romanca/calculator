61277734d1aefb2a4830427c96887a598bbc1509
romanca <r.jamrisko@gmail.com>
2021-10-29 11:57:57 +0200

    cypress auto test

---
+++ cypress/e2e/calc.spec.js
@@ -0,0 +1,8 @@
+describe("Transfers test", () => {
+  beforeEach(() => {
+    cy.visit("https://romanca.github.io/calculator/");
+    cy.viewport("macbook-15");
+  });
+
+  it("Calculation", () => {});
+});
